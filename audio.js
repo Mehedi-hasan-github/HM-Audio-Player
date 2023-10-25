0e4543a58d758e66dd5413b6a210d4cbe4756a4b
Mehedi-hasan-github <mehedy4884@gmail.com>
2023-10-25 14:33:10 +0600

    image gallary animation done

--- audio.js
+++ audio.js
@@ -1,3 +1,18 @@
 const audio = document.getElementById("audio");
 const progress = document.getElementById("progress");
 const playPause = document.getElementById("playPause");
+const imageGallary = document.getElementById("glrImage");
+
+function ctrlButton() {
+  if (playPause.classList.contains("fa-play")) {
+    playPause.classList.remove("fa-play");
+    playPause.classList.add("fa-pause");
+    imageGallary.classList.add("imageSpiner");
+    audio.play();
+  } else {
+    playPause.classList.remove("fa-pause");
+    playPause.classList.add("fa-play");
+    imageGallary.classList.remove("imageSpiner");
+    audio.pause();
+  }
+}
